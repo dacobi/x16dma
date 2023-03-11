7b9dbb61d5453c0b017e27f7fe0962a44c57d054
Jacob Kolding <dacobi@gmail.com>
2023-03-11 14:06:32 +0100

    indentation

--- top.v
+++ top.v
@@ -31,70 +31,73 @@ module top(
      
     always @(posedge PHI2) begin
             
-    if(TXEN == 1) begin
-        if(count > 0) begin
-            if(WRITE) begin
-                if(WAIT) begin
-                    tmpaddr = dadd;
-                    tmpdata <= dbyte;
-                    dadd <= dadd + dinc;
-                    WAIT <= 0;
-                end
-                else begin                    
-                    count <= count - 1;
-                    WRITE <= 0;
-                    WAIT <= 1;                               
-                end
-            end
-            else begin
-                if(WAIT) begin
-                    tmpaddr = sadd;
-                    sadd <= sadd + sinc;
-                    WAIT <= 0;
+        if(TXEN == 1) begin
+            if(count > 0) begin
+                if(WRITE) begin
+                    if(WAIT) begin
+                        tmpaddr = dadd;
+                        tmpdata <= dbyte;
+                        dadd <= dadd + dinc;
+                        WAIT <= 0;
+                    end
+                    else begin                    
+                        count <= count - 1;
+                        WRITE <= 0;
+                        WAIT <= 1;                               
+                    end
                 end
                 else begin
-                    dbyte <= data;
-                    WAIT <= 1;
-                    WRITE <= 1;                                        
-                end 
-            end
-        end 
-        else begin
-           TXEN <= 0;
-           WRITE <= 0;
-        end
-            
-        end
-        else begin
-        if(CS == 1) begin            
-            if(RWB == 1) begin
-                WRITE <= 0;
-                case(regaddr)
-                    5'b00000 : sadd[7:0] <= data;
-                    5'b00001 : sadd[15:8] <= data;
-                    5'b00010 : sinc[7:0] <= data;
-                    5'b00011 : sinc[15:8] <= data;
-                    5'b00100 : dadd[7:0] <= data;
-                    5'b00101 : dadd[15:8] <= data;
-                    5'b00110 : dinc[7:0] <= data;
-                    5'b00111 : dinc[15:8] <= data;
-                    5'b01000 : begin
-                            count[7:0] = data;
-                            if(count > 0) begin
-                                WAIT <= 1;
-                                TXEN <= 1;         
-                            end                                                                       
-                        end
-                    5'b01001 : count[15:8] <= data;
-                    default: TXEN <= 0;         
-                endcase         
-            end
-            end
+                    if(WAIT) begin
+                        tmpaddr = sadd;
+                        sadd <= sadd + sinc;
+                        WAIT <= 0;
+                    end
+                    else begin
+                        dbyte <= data;
+                        WAIT <= 1;
+                        WRITE <= 1;                                        
+                    end 
+                end
+            end 
             else begin
-                WRITE <= 1;
-                tmpdata <= 0;            
+                TXEN <= 0;
+                WRITE <= 0;
+            end            
+        end
+        else begin
+            if(CS == 1) begin            
+                if(RWB == 1) begin
+                    WRITE <= 0;
+                    case(regaddr)
+                        5'b00000 : sadd[7:0] <= data;
+                        5'b00001 : sadd[15:8] <= data;
+                        5'b00010 : sinc[7:0] <= data;
+                        5'b00011 : sinc[15:8] <= data;
+                        5'b00100 : dadd[7:0] <= data;
+                        5'b00101 : dadd[15:8] <= data;
+                        5'b00110 : dinc[7:0] <= data;
+                        5'b00111 : dinc[15:8] <= data;
+                        5'b01000 : begin
+                                count[7:0] = data;
+                                if(count > 0) begin
+                                    WAIT <= 1;
+                                    TXEN <= 1;         
+                                end                                                                       
+                                end
+                        5'b01001 : count[15:8] <= data;
+                        default: TXEN <= 0;         
+                    endcase         
+                end
+                else begin
+                    WRITE <= 1;
+                    tmpdata <= 0;            
+                end
+            end            
+            else begin
+                WRITE <= 0;
+                TXEN <= 0;
             end        
-      end      
+        end                  
     end
     
     assign regaddr = addr[4:0];
